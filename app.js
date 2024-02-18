04527b4a946a92cb60cb9e66a196be47f51c61f3
Nicolás Aguilera <60054614+nicoaguilerapy@users.noreply.github.com>
2024-02-18 19:58:59 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+require('dotenv').config(); // Para cargar las variables de entorno
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+const path = require('path');
+
+// Middleware para verificar el token de autorización
+const checkAuthorization = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token || token !== process.env.TOKEN) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    next();
+};
+
+// Función para verificar y crear el directorio si no existe
+const ensureDirectoryExists = (directory) => {
+    if (!fs.existsSync(directory)) {
+        try {
+            fs.mkdirSync(directory, { recursive: true });
+        } catch (error) {
+            console.error('Error al crear la carpeta:', error);
+            throw error;
+        }
+    }
+};
+
+app.post('/upload/:folder', checkAuthorization, upload.single('file'), (req, res) => {
+    const file = req.file;
+    if (!file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    
+    const folder = req.params.folder;
+    console.log('folder: ', folder);
+
+    // Define la ruta de la carpeta de destino
+    const destinationFolder = path.join(__dirname, 'uploads', folder);
+
+    try {
+        // Verifica y crea el directorio si no existe
+        ensureDirectoryExists(destinationFolder);
+
+        // Define la ruta de destino donde se moverá el archivo
+        const destinationPath = path.join(destinationFolder, file.originalname);
+
+        // Mueve el archivo a la carpeta de destino
+        fs.renameSync(file.path, destinationPath);
+        res.send('File uploaded successfully.');
+    } catch (error) {
+        console.error('Error al mover el archivo:', error);
+        res.status(500).send('Error al mover el archivo.');
+    }
+});
+
+
+
+
+
+// Ruta para recuperar imágenes
+app.get('/image/:folder/:imageName', (req, res) => {
+    const folder = req.params.folder;
+    const imageName = req.params.imageName;
+    const imagePath = path.join(__dirname, 'uploads', folder, imageName);
+
+    // Envía la imagen como respuesta
+    res.sendFile(imagePath);
+});
+
+app.get('/test', (req, res) => {
+    // Obtiene la fecha y hora actual
+    const currentDate = new Date();
+    // Crea un objeto JSON con la fecha, hora y estado activo
+    const responseJSON = {
+      date: currentDate.toLocaleDateString(),
+      time: currentDate.toLocaleTimeString(),
+      status: "active"
+    };
+    // Envía el objeto JSON como respuesta
+    res.json(responseJSON);
+});
+
+// Puerto en el que escucha el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
